585e8519e19763c5772e201c3f0111943807e355
hamid815 <federeco815@gmail.com>
2026-01-29 16:59:53 +0500

    Git refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Album from "./routes/Album";
+// import Album from "./routes/Album";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* bosh sahifa bo‘lsa albomga yo‘naltir */}
-        <Route path="/" element={<Navigate to="/a/8f3kLm92" />} />
+    // <BrowserRouter>
+    //   <Routes>
+    //     {/* bosh sahifa bo‘lsa albomga yo‘naltir */}
+    //     <Route path="/" element={<Navigate to="/a/8f3kLm92" />} />
 
-        {/* albom route */}
-        <Route path="/a/:albumId" element={<Album />} />
-      </Routes>
-    </BrowserRouter>
+    //     {/* albom route */}
+    //     <Route path="/a/:albumId" element={<Album />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <h1>Welcome to the Album App</h1>
   );
 }
 
