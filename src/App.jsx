ce2f40d351e656c7919b583d679c28263ac7bb9b
hamid815 <federeco815@gmail.com>
2026-01-29 15:44:41 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Album from "./routes/Album";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* bosh sahifa bo‘lsa albomga yo‘naltir */}
+        <Route path="/" element={<Navigate to="/a/8f3kLm92" />} />
+
+        {/* albom route */}
+        <Route path="/a/:albumId" element={<Album />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/routes/Album.jsx
@@ -0,0 +1,28 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export default function Album() {
+  const { albumId } = useParams();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`/data/${albumId}.json`)
+      .then(res => res.json())
+      .then(setData)
+      .catch(err => console.error(err));
+  }, [albumId]);
+
+  if (!data) return <h1>Loading...</h1>;
+
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.school}</p>
+      <p>{data.class}</p>
+
+      {data.gallery.map((img, i) => (
+        <img key={i} src={img} width="200" />
+      ))}
+    </div>
+  );
+}
